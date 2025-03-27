b6cf769cbf66bb1d96290d507c3a0383261ccbef
George Malak <georgmalak2004@gmail.com>
2025-03-27 02:56:09 +0200

    Add Load file feature git add .

--- Polynomial Operations.cpp
+++ Polynomial Operations.cpp
@@ -29,36 +29,43 @@ bool isValidNumber(const string &input) {
 class Polynomial {
     int degree;
     int *coefficients;
-    void splitString(string& input, int& index);
 
 public:
     Polynomial() = default;
-    explicit Polynomial(int degree);
+    explicit Polynomial(int degree , bool Help);
 
     ~Polynomial();
 
     static void displayMenu(Polynomial& polynomial1, const Polynomial &polynomial2);
     void add(const Polynomial &other);
+    void splitString(string& input, int& index);
     void subtract(const Polynomial &other);
     void displayPolynomial() const;
 };
 
 // ----------------------------------------------- CLASS IMPLEMENTATION
-
+//--------------------- Global Variable
+bool Status ;
 // --------------------- CONSTRUCTOR & DESTRUCTOR
 
-Polynomial::Polynomial(int degree) {
-    this->degree = degree;
-    this->coefficients = new int[degree + 2];
+Polynomial::Polynomial(int degree, bool IN) {
+    if(IN) {
+        this->degree = degree;
+        this->coefficients = new int[degree + 2];
 
-    if (degree > 0) {
-        cout << "Please, enter coefficients of Polynomial:";
-        string element;
+        if (degree > 0) {
+            cout << "Please, enter coefficients of Polynomial:";
+            string element;
 
-        // Handle int and long.
-        getline(cin, element);
-        int index = 0;
-        splitString(element, index);
+            // Handle int and long.
+            getline(cin, element);
+            int index = 0;
+            splitString(element, index);
+        }
+    }
+    else {
+        this->degree = degree;
+        this->coefficients = new int[degree + 2];
     }
 }
 
@@ -194,13 +201,10 @@ void Polynomial::displayMenu(Polynomial& polynomial1, const Polynomial& polynomi
     }
 }
 
-// ----------------------------------------------- MAIN FUNCTION
-
-int main() {
-    cout << "\n------------- WELCOME TO OUR POLYNOMIAL OPERATIONS APPLICATION -------------\n" << endl;
-
+// ----------------------------------------------- KIND OF I/P
+void runFromTerminal(){
     string degree1, degree2;
-
+    Status = true;
     while (true) {
         cout << "Please, enter order of First Polynomial: ";
         getline(cin, degree1);
@@ -208,7 +212,7 @@ int main() {
             cout << "Invalid input, Please enter a positive integer: ";
             getline(cin, degree1);
         }
-        Polynomial polynomial1(stoi(degree1));
+        Polynomial polynomial1(stoi(degree1) , Status);
 
         cout << "Please, enter order of Second Polynomial: ";
         getline(cin, degree2);
@@ -216,7 +220,7 @@ int main() {
             cout << "Invalid input, Please enter a positive integer: ";
             getline(cin, degree2);
         }
-        Polynomial polynomial2(stoi(degree2));
+        Polynomial polynomial2(stoi(degree2) , Status);
 
         Polynomial::displayMenu(polynomial1, polynomial2);
 
@@ -232,9 +236,119 @@ int main() {
 
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-        if (tolower(choice) == 'n') {
-            cout << "\nTHANKS FOR USING OUR APPLICATION :)" << endl;
-            return 0;
-        }
+        if (tolower(choice) == 'n') break;
     }
+}
+
+void runFromFile() {
+        string fileName;
+        Status = false ;
+        cout << "\nPlease, enter file name:";
+        while (true) {
+            getline(cin, fileName);
+            // Input the file and check from existing
+            if (fileName.size() < 5 || fileName.substr(fileName.size() - 4) != ".txt") {
+                cout << "\nThe file name should be like this ----> (file name).txt\n";
+                cout << "Please, enter a valid file name:";
+                continue;
+            }
+            ifstream file(fileName);
+            if (!file.is_open()) {
+                cout << "\nFile not found! Please, enter a valid file name:";
+                continue;
+            }
+
+            // Extract the coefficient and degree of polynomial
+            string line;
+            int poly_NO = 1;
+            while (getline(file, line)) {
+                if (line.empty()) continue;
+                cout << "\n======== Polynomial #" << poly_NO << " ========" << endl;
+
+                // Checker from 1st polynomial degree
+                if (!isValidNumber(line)) {
+                    cerr << "Error: Missing First polynomial degree in test case " << poly_NO << endl;
+                    continue;
+                }
+                // 1st polynomial will be extracted
+                Polynomial polynomial1(stoi(line) , Status);
+                getline(file, line);
+                int index1 = 0;
+                polynomial1.splitString(line, index1);
+
+                if (!getline(file, line)) {
+                    cerr << "Error: Missing second polynomial degree in test case " << poly_NO << endl;
+                    continue;
+                }
+                if (!isValidNumber(line)) {
+                    cerr << "Error: Degree of the second polynomial is invalid in test case " << poly_NO << endl;
+                    continue;
+                }
+                Polynomial polynomial2(stoi(line) , Status);
+                if (!getline(file, line)) {
+                    cerr << "Error: Missing coefficients for second polynomial in test case " << poly_NO << endl;
+                    continue;
+                }
+                // 1st polynomial will be extracted
+                int index2 = 0;
+                polynomial2.splitString(line, index2);
+
+                // Display them
+                cout << "First polynomial: ";
+                polynomial1.displayPolynomial();
+                cout << "Second polynomial: ";
+                polynomial2.displayPolynomial();
+                // Implement Sum - Subtract
+                polynomial1.add(polynomial2);
+                polynomial1.subtract(polynomial2);
+
+                poly_NO++;
+            }
+            file.close();
+            cout << "\n======== Done !! ========" << endl;
+            return;
+        }
+}
+// ----------------------------------------------- MAIN FUNCTION
+
+int main() {
+    cout << "\n------------- WELCOME TO OUR POLYNOMIAL OPERATIONS APPLICATION -------------\n" << endl;
+    string choice;
+    while (true) {
+        while (true) {
+            cout << "\nWhat do you want to do?" << endl;
+            cout << "1) Polynomial System." << endl;
+            cout << "2) Exit." << endl;
+            cout << "Please, enter your choice:";
+            getline(cin, choice);
+
+            // Check the validity of input.
+            if (choice == "1" || choice == "2") break;
+            cout << "Invalid choice. Please, Try again." << endl << endl;
+        }
+
+        // Exit the system.
+        if (choice == "2") break;
+        while (true) {
+            cout << "\nWhat do you want to do ?" << endl;
+            cout << "1) Run From Terminal." << endl;
+            cout << "2) Run From File." << endl;
+            cout << "3) Exit." << endl;
+            cout << "Please, enter your choice:";
+            getline(cin, choice);
+
+            // Check the validity of input.
+            if (choice == "1" || choice == "2" || choice == "3") break;
+            cout << "Invalid choice. Please, Try again." << endl << endl;
+        }
+
+        // Run from the terminal.
+        if (choice == "1") runFromTerminal();
+
+        // Run from the file.
+        else if (choice == "2") {
+            runFromFile();
+        } else continue;
+    }
+    cout << "\nTHANKS FOR USING OUR APPLICATION :)" << endl;
 }
